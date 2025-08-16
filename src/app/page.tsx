95f58e485cb55067db98af2ae5dbda7344a6c6c4
caesarKom <cezarykomorowski@gmail.com>
2025-08-16 22:11:12 +0200

    session-middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div className="flex flex-col gap-4 p-4">
-      <Input placeholder="Hello" />
-      <Button disabled>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Primary</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="teritary">Teritary</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="muted">Muted</Button>
+      Only visible to auth user.
+      <Button className="mt-4" onClick={() => mutate()}>
+        Logout
+      </Button>
     </div>
-  )
+  );
 }
