fb7038ce4127d140c51499254f7eb581aff7ea9f
caesarKom <cezarykomorowski@gmail.com>
2025-08-18 23:47:23 +0200

    responsive-modal

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,18 +1,19 @@
-import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { getWorkspace } from "@/features/workspaces/actions";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function HomePage() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
   if (!session) redirect("/sign-in");
 
-  return (
-    <div className="">
-      <CreateWorkspaceForm />
-    </div>
-  );
+  const workspaces = await getWorkspace();
+  if (workspaces.length === 0) {
+    redirect(`/workspaces/create`);
+  } else {
+    redirect(`/workspaces/${workspaces[0].id}`);
+  }
 }
--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -3,8 +3,8 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
 
-type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
+type ResponseType = InferResponseType<typeof client.api.workspaces["$post"], 200>
+type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>;
 
 export const useCreateWorkspace = () => {
   const queryClient = useQueryClient();
@@ -12,9 +12,11 @@ export const useCreateWorkspace = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ form }) => {
       const response = await client.api.workspaces["$post"]({ form });
+
       if (!response.ok) {
         throw new Error("Failed to create workspace");
       }
+
       return await response.json();
     },
     onSuccess: () => {
